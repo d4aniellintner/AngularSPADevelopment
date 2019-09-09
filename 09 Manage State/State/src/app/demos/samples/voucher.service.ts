38247b1fa99cf40feed0b6c1f8232dcd98e568a5
Alexander Pajer <alexander.pajer@integrations.at>
2019-09-09 11:12:29 +0200

    jsonserverbug

--- 09 Manage State/State/src/app/demos/samples/voucher.service.ts
+++ 09 Manage State/State/src/app/demos/samples/voucher.service.ts
@@ -9,30 +9,28 @@ export class VouchersService {
   constructor(private httpClient: HttpClient) {}
 
   getVouchers(): Observable<Voucher[]> {
-    return this.httpClient.get<Voucher[]>(environment.apiUrl + "api/vouchers");
+    return this.httpClient.get<Voucher[]>(environment.apiUrl + "vouchers");
   }
 
   getVoucher(id: number): Observable<Voucher> {
-    return this.httpClient.get<Voucher>(
-      environment.apiUrl + "api/vouchers/" + id
-    );
+    return this.httpClient.get<Voucher>(environment.apiUrl + "vouchers/" + id);
   }
 
   insertVoucher(voucher: Voucher): Observable<any> {
     return this.httpClient.post<Voucher>(
-      environment.apiUrl + "api/vouchers",
+      environment.apiUrl + "vouchers",
       voucher
     );
   }
 
   updateVoucher(voucher: Voucher): Observable<any> {
     return this.httpClient.put<Voucher>(
-      environment.apiUrl + "api/vouchers",
+      environment.apiUrl + "vouchers",
       voucher
     );
   }
 
   deleteVoucher(id: number): Observable<any> {
-    return this.httpClient.delete(environment.apiUrl + "api/vouchers/" + id);
+    return this.httpClient.delete(environment.apiUrl + "vouchers/" + id);
   }
 }
