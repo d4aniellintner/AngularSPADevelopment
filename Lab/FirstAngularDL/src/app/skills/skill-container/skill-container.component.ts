292f97fd2c91315a6c03dd323e592e16285386f3
d4niellintner <daniel.lintner@gmail.com>
2019-09-16 13:16:06 +0200

    i did root layout

--- Lab/FirstAngularDL/src/app/skills/skill-container/skill-container.component.ts
+++ Lab/FirstAngularDL/src/app/skills/skill-container/skill-container.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Skill } from '../model/skill.model';
 
 @Component({
-  selector: 'app-skill-container',
-  templateUrl: './skill-container.component.html',
-  styleUrls: ['./skill-container.component.scss']
+	selector: 'app-skill-container',
+	templateUrl: './skill-container.component.html',
+	styleUrls: [ './skill-container.component.scss' ]
 })
 export class SkillContainerComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	skills: Skill[] = [
+		{ id: 1, name: 'Typeskript', hours: 10, completed: false },
+		{ id: 2, name: 'Typeskrip2t', hours: 20, completed: false },
+		{ id: 3, name: 'Typeskript3', hours: 30, completed: false }
+	];
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
 
+	selectSkills(item: Skill) {
+		console.log(`You clicked ${item.name}`);
+	}
 }
