63ae1564946273acb80a8e7aaad38c6909ab7952
Alexander Pajer <alexander.pajer@integrations.at>
2019-09-18 10:35:01 +0200

    admin subpages

--- 04 Routing/Routing/src/app/admin/admin.module.ts
+++ 04 Routing/Routing/src/app/admin/admin.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminAComponent } from '../admin-a/admin-a.component';
+import { AdminBComponent } from '../admin-b/admin-b.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminAComponent, AdminBComponent],
   imports: [
     CommonModule
   ]
