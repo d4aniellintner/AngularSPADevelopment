68592689f155e74e46a4ce62ecaecf1bae92ad33
Alexander Pajer <alexander.pajer@integrations.at>
2019-09-18 14:57:10 +0200

    mat table edit

--- 05 UI/UserInterface/src/app/demos/samples/material-table/material-table.component.ts
+++ 05 UI/UserInterface/src/app/demos/samples/material-table/material-table.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit } from "@angular/core";
-import { MatTableDataSource } from "@angular/material";
-import { HttpClient } from "@angular/common/http";
-import { Voucher } from "../model";
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { HttpClient } from '@angular/common/http';
+import { Voucher } from '../model';
 
 @Component({
-  selector: "app-material-table",
-  templateUrl: "./material-table.component.html",
-  styleUrls: ["./material-table.component.scss"]
+  selector: 'app-material-table',
+  templateUrl: './material-table.component.html',
+  styleUrls: ['./material-table.component.scss']
 })
 export class MaterialTableComponent implements OnInit {
   dataSource: MatTableDataSource<Voucher>;
-  displayedColumns = ["Text", "Date", "Amount"];
+  displayedColumns = ['Text', 'Date', 'Amount', 'action'];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.http.get<Voucher[]>("./assets/vouchers.json").subscribe(data => {
+    this.http.get<Voucher[]>('./assets/vouchers.json').subscribe(data => {
       this.dataSource = new MatTableDataSource(data);
     });
   }
@@ -27,6 +27,6 @@ export class MaterialTableComponent implements OnInit {
   }
 
   editItem(row) {
-    console.log("Edit Row", row);
+    console.log('Edit Row', row);
   }
 }
