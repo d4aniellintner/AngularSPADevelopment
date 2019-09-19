5247983d426f0d08cd89716f4e8ad91e13c5439f
d4niellintner <daniel.lintner@gmail.com>
2019-09-19 13:43:05 +0200

    my changes

--- 07 Reactive Programming/Reactive/src/app/demos/samples/using-streams/using-streams.component.ts
+++ 07 Reactive Programming/Reactive/src/app/demos/samples/using-streams/using-streams.component.ts
@@ -1,34 +1,35 @@
-import { Component, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
-import { VouchersService } from "../voucher.service";
-import { Voucher } from "../model";
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { VouchersService } from '../voucher.service';
+import { Voucher } from '../model';
 
 @Component({
-  selector: "app-using-streams",
-  templateUrl: "./using-streams.component.html",
-  styleUrls: ["./using-streams.component.scss"]
+	selector: 'app-using-streams',
+	templateUrl: './using-streams.component.html',
+	styleUrls: [ './using-streams.component.scss' ]
 })
 export class UsingStreamsComponent implements OnInit {
-  constructor(private vs: VouchersService) {}
+	constructor(private vs: VouchersService) {}
 
-  //Classic Begginer Pattern
-  vouchers: Voucher[];
+	//Classic Begginer Pattern
+	vouchers: Voucher[];
 
-  //Stream Pattern
-  vouchers$: Observable<Voucher[]>;
+	//Stream Pattern
+	vouchers$: Observable<Voucher[]>;
 
-  ngOnInit() {
-    this.getDataClassic();
-    this.getDataStream();
-  }
+	ngOnInit() {
+		this.getDataClassic();
+		this.getDataStream();
+	}
 
-  getDataClassic() {
-    this.vs.getVouchers().subscribe(data => {
-      this.vouchers = data;
-    });
-  }
+	getDataClassic() {
+		this.vs.getVouchers().subscribe((data) => {
+			this.vouchers = data;
+		});
+	}
 
-  getDataStream() {
-    this.vouchers$ = this.vs.getVouchers();
-  }
+	getDataStream() {
+		this.vouchers$ = this.vs.getVouchers();
+		this.vouchers$.toPromise<Voucher[]>();
+	}
 }
